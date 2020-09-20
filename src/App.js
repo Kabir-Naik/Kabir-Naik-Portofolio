94057d0f955eac8c80630d72c2e7358c31195afb
Shridhar-dev <shridharkamat10@gmail.com>
2020-09-20 21:13:27 +0530

    first one

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import {Route ,Switch} from "react-router-dom"
+import Work from './Work';
+import Testimonials from './Testimonials';
+import About from './About';
+import ScrollToTop from './ScrollToTop';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScrollToTop />
+      <Switch>
+        <Route exact path="/"  component={Home} />
+        <Route path="/work" component={Work}/>
+        <Route path="/testimonials" component={Testimonials}/>
+        <Route path="/about" component={About}/>
+     </Switch>
     </div>
   );
 }
