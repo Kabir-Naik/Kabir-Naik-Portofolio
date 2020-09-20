c6adec8080f7b8b783793e491b53498f9bacda9a
Shridhar-dev <shridharkamat10@gmail.com>
2020-09-20 22:14:37 +0530

    responsive bugs fixed

--- src/TestItems.js
+++ src/TestItems.js
@@ -4,8 +4,8 @@ import kn from './Images/Kabir-Naik.jpg'
 function TestItems(props) {
     return (
         <div>
-            <div class="card" style={{width: '18rem'}}>
-                <div class="mx-5 my-5">
+            <div class="card mr-auto ml-auto">
+                <div class="mx-0 mx-xl-5 mx-md-5  my-5">
                 <div class="mx-5 ">    
                 <img src={props.cimage} class="card-img-top" alt="..." style={{borderRadius:"10rem"}}/>
                 </div>
--- src/Testimonials.js
+++ src/Testimonials.js
@@ -13,18 +13,18 @@ function Testimonials() {
             <h1 style={{fontSize:"3rem"}}>Our clients <i class="fas fa-heart" style={{color:"rgb(232,69,69)"}}></i> our services </h1>
             <div class="container">
                 <div class="row my-5">
-                    <div class="col">
+                    <div class="col py-2">
                         <TestItems cimage={kn} ctext="Simply Amazing"  person="Kabir Naik"/>
                     </div>
-                    <div class="col">
+                    <div class="col py-2">
                     <TestItems cimage={kn} ctext="Some quick example text to build on the card title and make up the bulk of the card's content." person="Kabir Naik"/>
 
                     </div>
-                    <div class="col">
+                    <div class="col py-2">
                     <TestItems cimage={kn} ctext="Some quick example text to build on the card title and make up the bulk of the card's content."  person="Kabir Naik"/>
 
                     </div>
-                    <div class="col">
+                    <div class="col py-2">
                     <TestItems cimage={kn} ctext="Some quick example text to build on the card title and make up the bulk of the card's content."  person="Kabir Naik"/>
 
                     </div>
