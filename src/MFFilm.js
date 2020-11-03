346439057ebee7fe90b9cb345333e35dda432fea
Shridhar-dev <shridharkamat10@gmail.com>
2020-11-03 17:17:00 +0530

    Data addition

--- src/MFFilm.js
+++ src/MFFilm.js
@@ -7,13 +7,13 @@ function MFFilm(props) {
             <h1 style={{fontSize:"3rem"}} class="pb-2">MY FILMS</h1>
             <div class="row mx-5 my-5 d-flex justify-content-center">
                 <div class="col-12 py-5 col-md-4 col-xl-4 zoomer">
-                   <a href={props.link1}> <img src={props.img1} class="img-fluid sva-img" width="250"/></a>
+                   <a href={props.link1}> <img src={props.img1} class="img-fluid sva-img" width="250px"/></a>
                 </div>
                 <div class="col-12 py-5 col-md-4 col-xl-4 zoomer">
-                <a href={props.link2}><img src={props.img2} class="img-fluid sva-img"  width="250"/></a>
+                <a href={props.link2}><img src={props.img2} class="img-fluid sva-img"  width="250px"/></a>
                 </div>
                 <div class="col-12 py-5 col-md-4 col-xl-4 zoomer">
-                <a href={props.link3}><img src={props.img3} class="img-fluid sva-img"  width="250"/></a>
+                <a href={props.link3}><img src={props.img3} class="img-fluid sva-img"  width="250px"/></a>
                 </div>
             </div>
         </div>
