ca6ae63cfdf8400258d93ef5dc000e84a51113f0
Shridhar-dev <shridharkamat10@gmail.com>
2020-11-09 11:14:29 +0530

    Data addition

--- src/TestItems.js
+++ src/TestItems.js
@@ -11,12 +11,14 @@ function TestItems(props) {
                 </div>
                 </div>
                 <div class="card-body">
-                    <p class="card-text" style={{color:"black"}}>{props.ctext}</p>
+                    <p class="card-text text-left" style={{color:"black"}}>{props.ctext}</p>
                 </div>
                 <div class="card-footer text-muted">
                     {props.person}
                 </div>
+                
             </div>
+          
         </div>
     )
 }
