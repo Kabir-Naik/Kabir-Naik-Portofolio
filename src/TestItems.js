3eea632b170ad12f2d995d4b6ba11e51cb8330c0
Shridhar-dev <shridharkamat10@gmail.com>
2021-04-30 22:19:38 +0530

    testimonial added

--- src/TestItems.js
+++ src/TestItems.js
@@ -3,22 +3,24 @@ import kn from './Images/Kabir-Naik.jpg'
 
 function TestItems(props) {
     return (
-        <div>
-            <div class="card mr-auto ml-auto">
-                <div class="mx-0 mx-xl-5 mx-md-5  my-5">
-                <div class="mx-5 ">    
-                <img src={props.cimage} class="card-img-top" alt="..." style={{borderRadius:"10rem"}}/>
+        <div class="col py-2">
+            <div>
+                <div class="card mr-auto ml-auto">
+                    <div class="mx-0 mx-xl-5 mx-md-5  my-5">
+                    <div class="mx-5 ">    
+                    <img src={props.cimage} class="card-img-top" alt="..." style={{borderRadius:"10rem"}}/>
+                    </div>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text text-left" style={{color:"black"}}>{props.ctext}</p>
+                    </div>
+                    <div class="card-footer text-muted">
+                        {props.person}
+                    </div>
+                    
                 </div>
-                </div>
-                <div class="card-body">
-                    <p class="card-text text-left" style={{color:"black"}}>{props.ctext}</p>
-                </div>
-                <div class="card-footer text-muted">
-                    {props.person}
-                </div>
-                
+            
             </div>
-          
         </div>
     )
 }
--- src/Testimonials.js
+++ src/Testimonials.js
@@ -11,6 +11,7 @@ import ok from './Images/omK.jpeg'
 import rb from './Images/rajatB.jpeg'
 import vr from './Images/vishalR.jpeg'
 import bk from './Images/brijeshK.jpeg'
+import td from './Images/tallulahD.jpeg'
 
 function Testimonials() {
     return (
@@ -18,35 +19,16 @@ function Testimonials() {
         <div class="container-fluid">
             <Navbar pc={true}/>
             <div class="container">
-                <div class="row my-5">
-                    <div class="col py-2">
-                        <TestItems cimage={omd} ctext="Kabir is young, passionate and enthusiastic and will definitely go a long way as a creator. His videos are very enriching and he tries to bring forward a different aspect of the Goan life, it's cultures and traditions in a simple yet informative way. His videos are worth a watch."  person="Omkar Dharwadkar – Wildlife Film maker / Naturalist"/>
-                    </div>
-                    <div class="col py-2">
+                <div class="row my-5">                    
+                    <TestItems cimage={omd} ctext="Kabir is young, passionate and enthusiastic and will definitely go a long way as a creator. His videos are very enriching and he tries to bring forward a different aspect of the Goan life, it's cultures and traditions in a simple yet informative way. His videos are worth a watch."  person="Omkar Dharwadkar – Wildlife Film maker / Naturalist"/>
                     <TestItems cimage={an} ctext="Kabir Naik is a deciplined guy who has a strong vision about things around us. The fact that he is trying to showcase our very own culture which is slowly fading away at this young age, earns him real respect. I'm pretty sure, his journey which started a few years would serve a lot to society and nature in coming years."  person="Amit Naik - Musician"/>
-                    </div>
-                    <div class="col py-2">
                     <TestItems cimage={ss} ctext="Working with Kabir has always been fascinating and a joy to learn something new may be in cinematography or may be in editing the master piece in his hand is just as how he thinks. I still remember when we actually met and discussed a script and we both agreed on doing it without any arguments because thats how filmmakers think and work together"  person="Sudesh Shet - Film Director & Producer"/>
-
-                    </div>
-                    <div class="col py-2">
                     <TestItems cimage={ok} ctext="Kabir Naik is one of the most proactive, dilligent and hardest working youngsters in the state. His films inspire me to do more in my life. Extremely proud of him for exploring all facets of Goan culture and helping inform the world about the rural traditions of our beautiful state"  person="Om Khandeparkar - Mass Communication Student"/>
-
-                    </div>
-                    <div class="col py-2">
                     <TestItems cimage={rb} ctext="I have seen efforts Kabir is taking to produce quality videos on various topics for quite a some time now. When most youngsters of his age are busy making tiktoks and reels, Kabir's strive to create content relate to nature and cleanliness is absolutely outstanding. I wish him good luck and hope he will create some award and heart winning content again and again"  person="Rajat Bakre - Owner T 4 tasty"/>
-
-                    </div>
-                    <div class="col py-2">
                     <TestItems cimage={vr} ctext="Kabir makes videos on everything about Goa that is worth knowing - all the big and important issues, of course, but also the small ordinary things. His topics are well researched and heart felt. Still a teenager, but as the most prolific YouTuber in the state, he has set an example for the other youth to follow."  person="Vishal Rawlley - Film maker / Artist"/>
-
-                    </div>
-                    <div class="col py-2">
                     <TestItems cimage={nm} ctext="Kabir is one of the most interesting kids I’ve ever met. I’ve seen a lot of passionate creators at his age. I like to think that I was the same way. But it is very rare to see someone so young with his level of artistic discipline and prowess. I love all the Goa based documentaries that he creates ( I send them to all my family Whatsapp groups), and I think he is truly inspirational, both for me and my peers, and for the next generation of Goan filmmakers." person="Noah Martins - Comedian, Host of DFTL Podcast"/>
-                    </div>
-                    <div class="col py-2">
                     <TestItems cimage={bk} ctext="One of the most passionate budding filmmaker of our state. I have seen Kabir grow over the years. The best thing I admire about him is the curiosity he has towards improving his craft, learning and experimenting with newer topics and overall being obsessed with telling stories that matter." person="Brijesh Kakodker - Film-maker"/>
-                    </div>
+                    <TestItems cimage={td} ctext="Over the years I have seen him mature and hone his skills. Kajugotto is my favorite and I have showcased it for my students of BBA as part of the environment studies programme. Besides this he has involved voluntarily for the save Mollem campaign raising key issues via films that he has made and awareness and reportage videos where he has spoken factually about the youth participation, action and various updates regarding the destruction of Goa's pristine forests." person="Tallulah D'Silva - Architect, Writer, Educator"/> 
                 </div>    
             </div>
             </div>
